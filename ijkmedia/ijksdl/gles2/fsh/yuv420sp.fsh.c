a966e7fac71771e98402800b101df741bd2c7b21
fogin <junjunfang200924@sohu-inc.com>
2022-04-12 10:45:10 +0800

    shader支持nv12(420sp)格式
    以h264_qsv解码验证通过。

--- ijkmedia/ijksdl/gles2/fsh/yuv420sp.fsh.c
+++ ijkmedia/ijksdl/gles2/fsh/yuv420sp.fsh.c
@@ -21,22 +21,48 @@
 #include "ijksdl/gles2/internal.h"
 
 static const char g_shader[] = IJK_GLES_STRING(
-    precision highp float;
-    varying   highp vec2 vv2_Texcoord;
-    uniform         mat3 um3_ColorConversion;
-    uniform   lowp  sampler2D us2_SamplerX;
-    uniform   lowp  sampler2D us2_SamplerY;
+	precision highp float;
+	varying   highp vec2 vv2_Texcoord;
+	uniform         mat3 um3_ColorConversion;
+	uniform			vec3 um3_rgbAdjustment;
+
+	uniform   lowp  sampler2D us2_SamplerX;
+	uniform   lowp  sampler2D us2_SamplerY;
+	uniform   lowp  sampler2D us2_SamplerZ;
+
+	uniform   int   isSubtitle;
+	uniform	  int   isFullRange;
+
+	vec3 rgb_adjust(vec3 rgb, vec3 rgbAdjustment) {
+		float B = rgbAdjustment.x;
+		float S = rgbAdjustment.y;
+		float C = rgbAdjustment.z;
+		rgb = (rgb - 0.5) * C + 0.5;
+		rgb = rgb + (0.75 * B - 0.5) / 2.5 - 0.1;
+		vec3 intensity = vec3(dot(rgb, vec3(0.299, 0.587, 0.114)));
+		return intensity + S * (rgb - intensity);
+	}
 
     void main()
     {
-        mediump vec3 yuv;
-        lowp    vec3 rgb;
+		if (isSubtitle == 1) {
+			gl_FragColor = texture2D(us2_SamplerX, vv2_Texcoord);
+		}
+		else {
+			mediump vec3 yuv;
+			lowp    vec3 rgb;
 
-        yuv.x  = (texture2D(us2_SamplerX,  vv2_Texcoord).r  - (16.0 / 255.0));
-        yuv.yz = (texture2D(us2_SamplerY,  vv2_Texcoord).rg - vec2(0.5, 0.5));
-        rgb = um3_ColorConversion * yuv;
-        gl_FragColor = vec4(rgb, 1);
-    }
+			yuv.x = (texture2D(us2_SamplerX, vv2_Texcoord).r - (16.0 / 255.0));
+			yuv.yz = (texture2D(us2_SamplerY, vv2_Texcoord).r - vec2(0.5, 0.5));
+
+			if (isFullRange == 1) {
+				yuv.x = yuv.x - 16.0 / 255.0;
+			}
+			rgb = um3_ColorConversion * yuv;
+			rgb = rgb_adjust(rgb, um3_rgbAdjustment);
+			gl_FragColor = vec4(rgb, 1);
+		}
+	}
 );
 
 const char *IJK_GLES2_getFragmentShader_yuv420sp()
